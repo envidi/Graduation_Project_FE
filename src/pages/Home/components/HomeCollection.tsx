67913fb40cc240d9092107bf4ce6eef284142316
thanhle030402 <lethithanh030402@gmail.com>
2024-03-03 11:16:10 +0700

    thanh

--- src/pages/Home/components/HomeCollection.tsx
+++ src/pages/Home/components/HomeCollection.tsx
@@ -18,7 +18,7 @@ export const HomeCollection = ({ dataMovie, isLoading }: MoviePropsType) => {
 
       // Tăng chỉ số của phim hiện tại lên 1, nếu đã ở phim cuối cùng, quay lại phim đầu tiên
       setCurrentMovieIndex((prevIndex) => (prevIndex + 1) % dataMovie.length);
-    }, 5000); // Thời gian chuyển đổi giữa các phim (đơn vị: ms), ở đây là 5 giây
+    }, 3000); // Thời gian chuyển đổi giữa các phim (đơn vị: ms), ở đây là 3 giây
 
     // Xóa interval khi component bị unmounted hoặc effect thay đổi
     return () => clearInterval(interval);
@@ -45,7 +45,7 @@ export const HomeCollection = ({ dataMovie, isLoading }: MoviePropsType) => {
       <div className="home-collection-container">
         {dataMovie && dataMovie.map((movie: MovieType, idx: number) => {
           // Kiểm tra xem phim này có phải là phim đang được hiển thị không, nếu có thì hiển thị, ngược lại ẩn đi
-          const isVisible = idx === currentMovieIndex;
+          const isVisible = idx >= currentMovieIndex && idx < currentMovieIndex + 3;
           return (
             <div key={idx} style={{ display: isVisible ? 'block' : 'none' }}>
               <CollectionCard className="" movie={movie} />
