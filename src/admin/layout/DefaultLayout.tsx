e89083c9efb60f4447c9d5395fbcec64424c9356
thanhle030402 <lethithanh030402@gmail.com>
2024-03-31 00:47:32 +0700

    feat: sua sidebar

--- src/admin/layout/DefaultLayout.tsx
+++ src/admin/layout/DefaultLayout.tsx
@@ -6,7 +6,7 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+    <div className="dark:bg-boxdark-2 dark:text-bodydark" style={{backgroundColor:'#eef2f6'}}>
       {/* <!-- ===== Page Wrapper Start ===== --> */}
       <div className="flex h-screen overflow-hidden">
         {/* <!-- ===== Sidebar Start ===== --> */}
