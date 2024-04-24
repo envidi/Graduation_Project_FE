816825704708900ee2e36f4048fdcd0e40904a6a
quagsulu <quangnvph24106@fpt.edu.vn>
2024-04-25 04:21:27 +0700

    fix: sửa lại disable button xóa và cập nhật

--- src/admin/pages/Movie/components/TableMovie.tsx
+++ src/admin/pages/Movie/components/TableMovie.tsx
@@ -239,7 +239,7 @@ const TableMovie = () => {
                           className="rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                           data-ripple-light="true"
                           // disabled={movie.status === 'IS_SHOWING'}
-                          disabled={movie.status === 'IS_SHOWING'}
+                          disabled={movie.status === 'IS_SHOWING' || movie.showTimes.length > 0}
                         >
                           Cập nhật
                         </button>
@@ -249,7 +249,7 @@ const TableMovie = () => {
                         className="rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         data-ripple-light="true"
                         onClick={() => handleShowConfirm(movie._id)}
-                        disabled={movie.status === 'IS_SHOWING'}
+                        disabled={movie.status === 'IS_SHOWING' || movie.showTimes.length > 0}
                       >
                         Xóa
                       </button>
