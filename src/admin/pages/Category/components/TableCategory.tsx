ce008fc686d261a6a9395949a50eaa38318d24d4
Chanhday03 <116981561+chanhday03@users.noreply.github.com>
2024-05-01 22:24:42 +0700

    fix: invalidateQuery category after adding

--- src/admin/pages/Category/components/TableCategory.tsx
+++ src/admin/pages/Category/components/TableCategory.tsx
@@ -23,10 +23,10 @@ const TableCategory = () => {
     mutationFn: removeCategory,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['CATEGORY'] })
-      toast.success('Xoa thành công')
+      toast.success('Xóa thành công')
     },
     onError: () => {
-      toast.error('Xoa that bai')
+      toast.error('Xóa thất bại')
     }
   })
 
